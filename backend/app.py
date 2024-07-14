f9bdeb33d40affc181529d7a532a9e8ec673cd00
Payton Webber <paytonwebber@gmail.com>
2024-07-14 00:39:57 -0700

    test: connecting payton and dhowa's backend to liam's frontend

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,36 @@
+import os
 from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.get("/test")
+    def test():
+        import tables
+        from .db import get_db
+        db = get_db()
+        return get_tasks(db)
+
+    from . import db
+    db.init_app(app)
 
     return app
 
